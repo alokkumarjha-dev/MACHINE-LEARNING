8719bc29d960a765174cafa877fd22c22d39636f
Alok Kumar jha <alokkumarjhahome@gmail.com>
2026-05-14 22:15:01 +0530

    Delete 1.PYTHON CODE/1.CONDITIONAL STATEMENT directory

--- 1.PYTHON CODE/1.CONDITIONAL STATEMENT/Logical Scenarios/Q8.py
+++
@@ -1,7 +0,0 @@
-'''
-Take a year and print the corresponding century (e.g., “19th century”, “20th century”)
-'''
-
-year = int(input("Enter year : "))
-ce = (year//100)+1
-print(year,"falls on",ce,"century")
--- 1.PYTHON CODE/1.CONDITIONAL STATEMENT/Nested If & Multiple Conditions/Q6.py
+++
@@ -1,9 +0,0 @@
-'''
-Check voting eligibility for a given age (18+). 
-'''
-
-a = int(input("Enter your Age : "))
-if a >= 18:
-    print("You are Eligible for vote!")
-else:
-    print("You are not Eligible for vote!")
