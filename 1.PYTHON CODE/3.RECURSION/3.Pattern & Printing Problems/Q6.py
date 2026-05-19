16456f46d0fb4e9e792cc3d5ba73503237b033fc
Alok Kumar jha <alokkumarjhahome@gmail.com>
2026-05-19 21:01:57 +0530

    Add files via upload

---
+++ 1.PYTHON CODE/3.RECURSION/3.Pattern & Printing Problems/Q6.py
@@ -0,0 +1,10 @@
+'''
+Print a triangle of stars recursively (bottom-up).
+'''
+
+n = int(input("Enter n : "))
+def star(i,n):
+    if i>n:
+        return ""
+    return  star(i+1,n) +" "*(n-i)+ "*"*i + "\n"
+print(star(1,n))
